0424f47bd9f297ee287a68eeaf28d5d07c617982
Harman Sahota <56659047+Harman-Sahota@users.noreply.github.com>
2022-08-14 15:01:44 -0400

    Add files via upload

---
+++ Shared/Calculator_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_iosApp.swift
+//  Shared
+//
+//  Created by Harman Sahota on 2022-08-14.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
